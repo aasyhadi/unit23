9fffc01d4d62ff2c37b6416edf3b4e3d7e33be5b
aasyhadi <adiproojambi@gmail.com>
2023-03-08 18:09:48 +0700

    d

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,27 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (env('PUBLIC_PATH') !== NULL) {
+            //An example that demonstrates setting Laravel's public path.
+            $this->app['path.public'] = env('PUBLIC_PATH');
+
+            // An example that demonstrates setting a third-party config value.
+            //$this->app['config']
+            //  ->set('cartalyst.themes.paths',
+            //      array(env('PUBLIC_PATH') . DIRECTORY_SEPARATOR . 'themes'));
+        } else {
+            $this->app['path.public'] = base_path() . '/../public';
+        }
+
+        // Possible environment changes
+        if ($this->app->environment() === 'local') {
+
+        } elseif ($this->app->environment() === 'development') {
+
+        } elseif ($this->app->environment() === 'test') {
+
+        } elseif ($this->app->environment() === 'production') {
+
+        }
     }
 }
